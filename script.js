4e04caab8fbebb4908176f5c8c949c988ef44b27
IT21314742 <it21314742@my.sliit.lk>
2024-04-16 10:35:03 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let toastBox = document.getElementById('toastBox');
 let successMsg = '<i class="fa-solid fa-circle-check"></i>Successfully Submitted';
-let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!'
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
