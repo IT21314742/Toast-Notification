3e09d8e7277640839a3ab58191fcb1923519d60c
IT21314742 <it21314742@my.sliit.lk>
2024-04-17 19:57:21 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
 let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, check again';
 
 function showToast(msg) {
-    
+    let
 }
