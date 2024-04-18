8cf3f4414623ae29a51b7558b138082890b00e27
IT21314742 <it21314742@my.sliit.lk>
2024-04-18 11:11:39 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ function showToast(msg) {
         toast.classList.add('error');
     }
     if(msg.includes('Invalid')){
-        toast.classList.add('I')
+        toast.classList.add('In')
     }
 }
